eb94026bb4febff29b3f6b92cdad6a44776796f9
Carolina Diaz <cdiaza87@gmail.com>
2022-02-01 15:11:59 -0300

    Last commit

---
+++ src/whilePorDoWhile/CambiarWhile.java
@@ -0,0 +1,16 @@
+package whilePorDoWhile;
+
+public class CambiarWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		    int i=0;
+		        do { 
+		            i+=1;
+		            System.out.printf("Iteración %d\n",i);
+		        }
+		        while (i<50);
+		        }       
+		    }
